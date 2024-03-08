b98b54f91037a1cb3e182a3da69f95894d10becb
Jain <shashank.jain01@sap.com>
2024-03-08 10:07:34 +0530

    initial commit

--- worker.py
+++ worker.py
@@ -1,13 +1,12 @@
 from rq import Worker, Queue, Connection
 from redis import Redis
 
-# Setup Redis connection
-redis_host = "db-redis-nyc3-93249-do-user-4571001-0.c.db.ondigitalocean.com"
-redis_port = 25061  # This should be an integer, e.g., 6379
-redis_username = "default"  # Redis >=6 supports username
-redis_password = "AVNS_xgDxBgvDmNRvM6wN0ti"
-
+redis_host = os.getenv('REDIS_HOST', 'default_host')
+redis_port = int(os.getenv('REDIS_PORT', 25061))  # Default Redis port
+redis_username = os.getenv('REDIS_USERNAME', 'default')
+redis_password = os.getenv('REDIS_PASSWORD', ''
 redis_conn = Redis(host=redis_host, port=redis_port, username=redis_username, password=redis_password, ssl=True, ssl_cert_reqs=None)
+#q = Queue(connection=redis_conn)
 
 # Specify the list of queues the worker will listen to
 queues_to_listen = ['default']
