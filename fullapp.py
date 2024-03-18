b959d9438870c497eea945e57a10059280566db6
Jain <shashank.jain01@sap.com>
2024-03-18 14:50:33 +0530

    make job handling on demand for infer

--- fullapp.py
+++ fullapp.py
@@ -447,6 +447,7 @@ def start_infer():
             new_filename = f"{uuid.uuid4()}{file_extension}"  # Generates a new filename with original extension
             filepath = os.path.join(UPLOAD_FOLDER, secure_filename(new_filename))
             file.save(filepath)
+            time.sleep(10)
             app.logger.error(f'file saved with filepath =: {filepath}')
             # Adjusted to pass filepath and speaker_name to the main function
             app.logger.info("enqueed the job ")
