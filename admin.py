b35523329db97f77a28a13ad82a5ec42f5b3f62d
Jain <shashank.jain01@sap.com>
2024-03-20 07:20:00 +0530

    make job handling on demand for infer

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, request, jsonify, session,render_template,flash,jsonify,send_file
 from functools import wraps
 from redis import Redis
 import os
